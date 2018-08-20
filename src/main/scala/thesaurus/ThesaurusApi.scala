903e82a809401ee1fea0e507efa7d674f706c7e7
empi <jurij.jurich@gmail.com>
2018-08-20 12:10:32 +0200

    Extracted methods from receive in API actor

--- src/main/scala/thesaurus/ThesaurusApi.scala
+++ src/main/scala/thesaurus/ThesaurusApi.scala
@@ -7,18 +7,45 @@ import akka.stream.Materializer
 import spray.json._
 
 import concurrent.duration._
+import scala.concurrent.Future
 import scala.util.matching.Regex
 
 object ThesaurusApi {
 	def props(implicit materializer: Materializer) = Props(new ThesaurusApi())
 
-	private def constructRequest(term: String): HttpRequest = HttpRequest(uri = s"https://www.thesaurus.com/browse/$term")
-
 	private val strictTimeout: FiniteDuration = 3 seconds
 
+	private val expressionHead = """<script>window.INITIAL_STATE = """
+	private val jsonSelectionExpr: Regex = (expressionHead + """\{.*\}""").r
+
+	private def constructRequest(term: String): HttpRequest = HttpRequest(uri = s"https://www.thesaurus.com/browse/$term")
+
+	private def parseExpression(expression: Regex, headSkip: Int)(input: String): JsValue = {
+		expression.findFirstIn(input) match {
+			case Some(jsonSelection) => jsonSelection.substring(headSkip).parseJson
+			case None => throw new RuntimeException("Exception raised during parsing json")
+		}
+	}
+
+	private def extractJsValue(path: List[String])(jsValue: JsValue): JsValue = path match {
+		case Nil => jsValue
+		case ::(head, tl) => jsValue match {
+			case JsObject(fields) => extractJsValue(tl)(fields(head))
+			case JsArray(elements) => extractJsValue(path)(elements.head)
+		}
+	}
+
+	private def extractField(path: List[String], fieldName: String)(jsValue: JsValue): Vector[String] = {
+		extractJsValue(path)(jsValue) match {
+			case JsArray(elements) => elements.map {
+				case JsObject(fields) => fields(fieldName).toString
+			}
+		}
+	}
+
 	private case class Word(term: String,
-									synonyms: Seq[String], antonyms: Seq[String],
-									usageExamples: Seq[String]) extends ThesaurusWord
+													synonyms: Seq[String], antonyms: Seq[String],
+													usageExamples: Seq[String]) extends ThesaurusWord
 }
 
 class ThesaurusApi(implicit materializer: Materializer) extends Actor {
@@ -28,43 +55,16 @@ class ThesaurusApi(implicit materializer: Materializer) extends Actor {
 
 	import ThesaurusApi._
 
-
 	override def receive: Receive = {
 		case WordLookup(term) =>
-
-			val expressionHead = """<script>window.INITIAL_STATE = """
-			val jsonSelectionExpr: Regex = """<script>window.INITIAL_STATE = \{.*\}""".r
-
-			val futureData = for {
-				HttpResponse(_, _, entity, _) <- Http().singleRequest(constructRequest(term))
-				strict <- entity.toStrict(strictTimeout)
-			} yield strict.data.utf8String
-
-			val parsedJson = futureData.map(jsonSelectionExpr.findFirstIn(_) match {
-				case Some(jsonSelection) => jsonSelection.substring(expressionHead.length).parseJson
-				case None => throw new RuntimeException("Exception raised during parsing json")
-			})
-
+			val parsedJson = responseData(constructRequest(term)).map(parseExpression(jsonSelectionExpr, expressionHead.length))
 			val wordData = parsedJson.map(extractJsValue(List("searchData", "tunaApiData")))
 
-			val synonyms = wordData.map(extractJsValue(List("posTabs", "synonyms"))(_) match {
-				case JsArray(elements) => elements.map {
-					case JsObject(fields) => fields("term").toString
-				}
-			})
+			val fieldExtractor = (path: List[String], field: String) => wordData.map(extractField(path, field))
 
-			val antonyms = wordData.map(extractJsValue(List("posTabs", "antonyms"))(_) match {
-				case JsArray(elements) => elements.map {
-					case JsObject(fields) => fields("term").toString
-				}
-			})
-
-			val examples = wordData.map(extractJsValue(List("exampleSentences"))(_) match {
-				case JsArray(elements) => elements.map {
-					case JsObject(fields) => fields("sentence").toString
-				}
-			})
-
+			val synonyms = fieldExtractor(List("posTabs", "synonyms"), "term")
+			val antonyms = fieldExtractor(List("posTabs", "antonyms"), "term")
+			val examples = fieldExtractor(List("exampleSentences"), "sentence")
 
 			val futureWord = for {
 				s <- synonyms
@@ -75,13 +75,10 @@ class ThesaurusApi(implicit materializer: Materializer) extends Actor {
 			sender() ! futureWord
 	}
 
-
-	private def extractJsValue(path: List[String])(jsValue: JsValue): JsValue = path match {
-		case Nil => jsValue
-		case ::(head, tl) => jsValue match {
-			case JsObject(fields) => extractJsValue(tl)(fields(head))
-			case JsArray(elements) => extractJsValue(path)(elements.head)
-		}
+	private def responseData(request: HttpRequest): Future[String] = {
+		for {
+			HttpResponse(_, _, entity, _) <- Http().singleRequest(request)
+			strict <- entity.toStrict(strictTimeout)
+		} yield strict.data.utf8String
 	}
-
 }
