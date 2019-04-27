224ba3a4282df794952171ec12a8b45fd7d5c55e
Yura Slinkin <jurij.jurich@gmail.com>
2019-04-27 23:56:36 +0200

    Basic description of api

--- src/main/scala/thesaurus/api/package.scala
+++ src/main/scala/thesaurus/api/package.scala
@@ -4,6 +4,6 @@ package object api {
 
   case class ThesaurusWord(term: String, meanings: Seq[WordMeaning], usageExamples: Seq[String])
 
-  case class WordMeaning(definition: String, synonyms: Seq[String], antonyms: Seq[String])
+  case class WordMeaning(context: String, synonyms: Seq[String], antonyms: Seq[String])
 
 }
